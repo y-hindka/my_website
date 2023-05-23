6d288cc59a70421a199510c1b8c37dfc80073790
Yash H <yhindka@gmail.com>
2023-05-23 01:11:53 -0500

    made more progress

--- js/page_transitions.js
+++ js/page_transitions.js
@@ -1,9 +1,29 @@
-function wipe_transition(event)
+var transitionLen = 500; // 0.5 seconds
+
+window.onload = override_window_on_load();
+
+function override_window_on_load()
+{
+    // update navbar
+    var active = document.getElementById(document.getElementById("PageName").textContent);
+    active.style.color = "darkgrey";
+    active.style.borderBottom = "1pt solid darkgrey";
+
+    slide_transition_in();
+}
+
+function slide_transition_in()
+{
+    window.setTimeout(function() {
+        document.getElementById("PageTransition").classList.remove("is-active");
+    }, transitionLen);
+}
+
+function slide_transition_out(event)
 {
     event.preventDefault();
-    document.getElementById("PageTransition").classList.add("WipeAnimation");
+    document.getElementById("PageTransition").classList.add("is-active");
     window.setTimeout(function() {
-        //window.location.href = event.target.href;
-        
-    }, 2000);
-};
+        window.location.href = event.target.href;
+    }, transitionLen);
+}
