aeff99482628a5035a94ec4d3cddcc74a2587dc1
Yash H <yhindka@gmail.com>
2023-06-11 13:24:11 -0400

    done with front page and resume pages

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,28 @@
-var currSlide = 0;
-var prevSlide = null;
+var carouselContainer = document.getElementById("Carousel-container");
 var containers = document.getElementsByClassName("Container");
+var currActive = 1;
 
-update_carousel();
+carouselContainer.addEventListener('scroll', function () {
+    for (var i = 0; i < containers.length; i++)
+    {
+        if (Math.abs(containers[i].getBoundingClientRect().left - carouselContainer.getBoundingClientRect().left) <= 75)
+        {
+            containers[currActive].classList.remove("active");
+            containers[i].classList.add("active");
+            currActive = i;
+        }
+    }
+});
 
-function update_carousel() {
-    if (prevSlide != null) containers[prevSlide].style.display = "none";
-    containers[currSlide].style.display = "inline-block";
-}
+var pianoImg = document.getElementById("piano_img");
+var pianoVid = document.getElementById("piano_vid");
 
-function round_clicked(increment) {
-    prevSlide = currSlide;
-    currSlide = (currSlide + increment) % containers.length;
-    if (currSlide < 0) currSlide = containers.length - 1;
-    update_carousel();
-}
+pianoImg.addEventListener('mouseenter', function() {
+    pianoVid.style.display = 'inherit';
+    pianoImg.style.display = 'none';
+});
+
+pianoVid.addEventListener('mouseleave', function() {
+    pianoVid.style.display = 'none';
+    pianoImg.style.display = 'inherit';
+});
