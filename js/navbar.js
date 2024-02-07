0ac984325967aea7c45fc9a55f413caa2213dd61
Yash H <yhindka@gmail.com>
2024-02-07 16:29:31 -0500

    revamped website

---
+++ js/navbar.js
@@ -0,0 +1,57 @@
+var scroll_top = 0;
+
+function navbar_scroll()
+{
+    // retrieve top NavBar
+    var navbar_top = document.getElementById("NavBarTop");
+    var threshold = navbar_top.getBoundingClientRect().height / 2;
+
+    // retrieve scrolled NavBar
+    var navbar_scrolled = document.getElementById("NavBarScrolled");
+    var page_links = document.getElementById("NavBarPageLinks")
+
+    // retrieve scroll position
+    var scroll_pos = window.scrollY;
+
+    if ( scroll_pos <= 0 || scroll_pos <= threshold )
+    {
+        // change NavBar styling if scrolled all the way at the top
+
+        // show top NavBar
+        navbar_top.style.visibility = 'visible';
+
+        // hide smaller, scrolled NavBar
+        navbar_scrolled.style.visibility = 'hidden';
+        navbar_scrolled.style.top = '-100%';
+    }
+    else if ( scroll_pos > scroll_top )
+    {
+        // change NavBar styling if scrolling down
+
+        // hide top NavBar
+        navbar_top.style.visibility = 'hidden';
+
+        // show smaller, scrolled NavBar
+        navbar_scrolled.style.visibility = 'visible';
+        navbar_scrolled.style.top = '0';
+
+        // make links invisible
+        page_links.classList.remove("display-contents");
+        page_links.classList.add("display-none");
+    }
+    else if ( scroll_pos < scroll_top)
+    {
+        // change NavBar styling if scrolling up
+
+        // make links visible
+        page_links.classList.remove("display-none");
+        page_links.classList.add("display-contents");
+    }
+
+    scroll_top = scroll_pos;
+}
+
+document.addEventListener(
+    'scroll',
+    navbar_scroll
+);
--- js/utilities.js
+++ js/utilities.js
@@ -1,10 +1,9 @@
+var animLen = 1000; // 1 second
+
 function comeIn(elems, threshold = 0) {
     var windowPos = window.scrollY + window.innerHeight;
     for (var i = 0; i < elems.length; i++)
     {
-        if (elems[i].offsetTop - windowPos < threshold)
-        {
-            elems[i].classList.add("comeIn");
-        }
+        if (elems[i].offsetTop - windowPos < threshold) elems[i].classList.add("comeIn");
     }
 }
